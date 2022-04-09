ca17f6696c188e63e9bbf4de2f6cf345b2a9f2d9
Zainal21 <Zainalarifin080718@gmail.com>
2022-04-09 16:17:57 +0700

    chore : add depedencies

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org" className="font-bold text-gray-800">Cikajogja!!</a>
         </h1>
 
         <p className={styles.description}>
@@ -38,16 +38,6 @@ export default function Home() {
           >
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
           </a>
         </div>
       </main>

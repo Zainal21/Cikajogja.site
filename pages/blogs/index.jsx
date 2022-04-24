7b4bf82deb2f893b0af9a8f860646d7d28b17e34
Zainal21 <Zainalarifin080718@gmail.com>
2022-04-24 08:02:47 +0700

    chore : config formatter

--- pages/blogs/index.jsx
+++ pages/blogs/index.jsx
@@ -1,20 +1,20 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Blogs() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-         <title>Blogs | Cikajogja</title>
+        <title>Blogs | Cikajogja</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-3xl font-bold">
-          Cooming Soon {' '}
+          Cooming Soon{" "}
           <a className="text-red-600" href="https://nextjs.org">
             Blog!
           </a>
         </h1>
       </main>
     </div>
-  )
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -8,13 +8,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-          <h1 className="text-3xl font-bold">
-          Cooming Soon {' '}
-            <a className="text-red-600" href="https://nextjs.org">
-               New Cikajogja.com!
-            </a>
-          </h1>
+        <h1 className="text-3xl font-bold">
+          Cooming Soon{" "}
+          <a className="text-red-600" href="https://nextjs.org">
+            New Cikajogja.com!
+          </a>
+        </h1>
       </main>
     </div>
-  )
+  );
 }
